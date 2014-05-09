eb3026b885c707cb2fa6d91a0faf0349ebb917a0
Daniel.A <danielas13@ru.is>
2014-05-09 15:33:39 +0000

    Bætti við Views og Controllers

---
+++ verklega/verklega/Controllers/SubtitleController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace verklega.Controllers
+{
+    public class SubtitleController : Controller
+    {
+        //
+        // GET: /Subtitle/
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult AddSubtitle()
+        {
+            return View();
+        }
+        public ActionResult EditSubtitle()
+        {
+            return View();
+        }
+        public ActionResult SearchSubtitle()
+        {
+            return View();
+        }
+        public ActionResult ViewSubtitle()
+        {
+            return View();
+        }
+	}
+}
